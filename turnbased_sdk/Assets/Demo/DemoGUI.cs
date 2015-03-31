f6b1ec6639fad27a0b355b336e1c5d5c6cd3de3e
zac <zac@playfab.com>
2015-03-31 16:55:38 -0700

    new photon code (move over to use the turn based SDK only).

---
+++ turnbased_sdk/Assets/Demo/DemoGUI.cs
@@ -0,0 +1,316 @@
+using ExitGames.Client.Photon.LoadBalancing;
+using System.Collections.Generic;
+using UnityEngine;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+public class DemoGUI : MonoBehaviour
+{
+    public DemoGame GameInstance;
+    public string AppId;            //set this in the editor in a scene!
+    public Rect LobbyRect;  // set in inspector to position the lobby screen
+    public Rect leftToolbar;  // set in inspector to position the lobby screen
+	
+
+	public int gui_depth = 2;
+	public PlayFabIntegration playfabComponent;
+	
+    public void Start()
+    {
+        Application.runInBackground = true;
+        CustomTypes.Register();
+
+        leftToolbar = new Rect(leftToolbar.x, leftToolbar.y, leftToolbar.width, Screen.height - leftToolbar.y);
+
+        this.GameInstance = new DemoGame();
+        this.GameInstance.AppId = this.AppId;       // set in Inspector
+        this.GameInstance.AppVersion = "1.0";
+        this.GameInstance.PlayerName = !string.IsNullOrEmpty(NamepickerGui.NickName) ? NamepickerGui.NickName : "unityPlayer";
+
+        this.GameInstance.OnStateChangeAction += this.OnStateChanged;
+        
+		playfabComponent.LoginToPlayFab();
+			
+	}
+
+	public void ConnectToMasterServer(string id, string ticket)
+	{	
+		if(this.GameInstance.CustomAuthenticationValues != null)
+		{
+			this.GameInstance.CustomAuthenticationValues.SetAuthParameters(id, ticket);
+		}
+		else
+		{
+			//Debug.Log(string.Format("Id: {0}, Ticket: {1}", id, ticket));
+			this.GameInstance.CustomAuthenticationValues = new AuthenticationValues()
+			{
+				AuthType = CustomAuthenticationType.Custom,
+				Secret = null,
+				AuthParameters = null,
+			};
+			
+			this.GameInstance.CustomAuthenticationValues.SetAuthParameters(id, ticket);
+		}
+		this.GameInstance.ConnectToRegionMaster("US");  // Turnbased games have to use this connect via Name Server
+	}
+
+
+    private void OnStateChanged(ClientState state)
+    {
+        if (state == ClientState.ConnectedToMaster)
+        {
+            this.GetRoomsList();
+        }
+    }
+
+    private void GetRoomsList()
+    {
+        this.GameInstance.OpWebRpc("GetGameList", new Dictionary<string, object>());
+    }
+
+    public void OnApplicationQuit()
+    {
+        if (this.GameInstance != null && this.GameInstance.loadBalancingPeer != null)
+        {
+            this.GameInstance.Disconnect();
+            this.GameInstance.loadBalancingPeer.StopThread();
+        }
+        this.GameInstance = null;
+    }
+
+    public void Update()
+    {
+        this.GameInstance.Service();
+
+        // "back" button of phone will quit
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        } 
+    }
+
+    public void OnGUI()
+    {
+        if (string.IsNullOrEmpty(this.AppId))
+        {
+            GUILayout.Label("You must enter your AppId from the Dashboard in the component: Scripts, DemoGUI.AppId before you can use this demo.");
+            return;
+        }
+
+        GUI.skin.button.stretchWidth = true;
+        GUI.skin.button.fixedWidth = 0;
+
+        GUILayout.Label("name: " + this.GameInstance.PlayerName + " state: " + GameInstance.State.ToString());
+
+        if (!string.IsNullOrEmpty(this.GameInstance.ErrorMessageToShow))
+        {
+            GUILayout.Label(this.GameInstance.ErrorMessageToShow);
+        }
+
+        switch (GameInstance.State)
+        {
+            case ClientState.JoinedLobby:
+                this.GuiInLobby();
+                break;
+            case ClientState.Joined:
+                this.GuiInGame();
+                break;
+            case ClientState.Disconnected:
+                if (GUILayout.Button("Connect"))
+                {
+					playfabComponent.LoginToPlayFab();
+					//this.GameInstance.ConnectToRegionMaster("US");  // Turnbased games have to use this connect via Name Server
+                    
+                }
+                break;
+        }
+    }
+
+    private void GuiInLobby()
+    {
+        GUILayout.BeginArea(LobbyRect);
+        GUILayout.Label("Lobby Screen");
+        GUILayout.Label(string.Format("Players in rooms: {0} looking for rooms: {1}  rooms: {2}", this.GameInstance.PlayersInRoomsCount, this.GameInstance.PlayersOnMasterCount, this.GameInstance.RoomsCount));
+        
+        if (GUILayout.Button("Join Random (or create)"))
+        {
+            this.GameInstance.OpJoinRandomRoom(null, 0);
+        }
+        if (GUILayout.Button("Create New Game"))
+        {
+            this.GameInstance.CreateTurnbasedRoom();
+        }
+        GUILayout.Space(20);
+
+        GUILayout.Label("Saved Games: " + this.GameInstance.SavedGames.Count);
+        foreach (KeyValuePair<string, int> savedRoom in this.GameInstance.SavedGames)
+        {
+            string roomName = savedRoom.Key;
+            int actorNumber = savedRoom.Value;
+            if (GUILayout.Button("ReJoin: " + roomName + " #" + actorNumber))
+            {
+                this.GameInstance.OpJoinRoom(roomName, actorNumber);
+            }
+        }
+
+        if (GUILayout.Button("Refresh", GUILayout.Width(150)))
+        {
+            this.GetRoomsList();
+        }
+        GUILayout.Space(20);
+
+        GUILayout.Label("Rooms in lobby: " + this.GameInstance.RoomInfoList.Count);
+        foreach (RoomInfo roomInfo in this.GameInstance.RoomInfoList.Values)
+        {
+            if (GUILayout.Button(roomInfo.Name + " turn: " + roomInfo.CustomProperties["t#"]))
+            {
+                this.GameInstance.OpJoinRoom(roomInfo.Name);
+            }
+        }
+
+        GUILayout.EndArea();
+    }
+
+    private void GuiInGame()
+    {
+        GUILayout.BeginArea(leftToolbar);
+        GUI.skin.button.stretchWidth = false;
+        GUI.skin.button.fixedWidth = 150;
+
+        // we are in a room, so we can access CurrentRoom and it's Players
+        GUILayout.Label("In Room: " + this.GameInstance.CurrentRoom.Name);
+        string interestingPropsAsString = FormatRoomProps();
+        if (!string.IsNullOrEmpty(interestingPropsAsString))
+        {
+            GUILayout.Label("Props: " + interestingPropsAsString);
+        }
+
+        foreach (Player player in this.GameInstance.CurrentRoom.Players.Values)
+        {
+            if (player.ID == this.GameInstance.lastTurnPlayer)
+            {
+                GUILayout.Label(player.ToString() + " (played last)");
+            }
+            else
+            {
+                GUILayout.Label(player.ToString());
+            }
+        }
+
+        GUILayout.Space(15);
+
+        GUILayout.Label("Save the board by ending the turn.");
+        if (GUILayout.Button("End Turn " + this.GameInstance.turnNumber + " (Save)"))
+        {
+            this.GameInstance.SaveBoardAsProperty();
+        }
+
+        GUILayout.Space(15);
+        if (this.GameInstance.CurrentRoom.IsVisible)
+        {
+            GUILayout.Label("Start game to avoid more joins.");
+            if (GUILayout.Button("Start (hide in lobby)"))
+            {
+                this.GameInstance.CurrentRoom.IsVisible = false;
+            }
+        }
+        else
+        {
+            GUILayout.Label("Accept more players by matchmaking.");
+            if (GUILayout.Button("Show in Lobby"))
+            {
+                this.GameInstance.CurrentRoom.IsVisible = true;
+            }
+        }
+
+        GUILayout.Space(15);
+
+        GUILayout.Label("To test, set some room/user props.");
+        // creates a random property of this room with a random value to set
+        if (GUILayout.Button("Set Room Property"))
+        {
+            Hashtable randomProps = new Hashtable();
+            randomProps[RandomCustomRoomProp()] = Random.Range(0, 99);
+            this.GameInstance.CurrentRoom.SetCustomProperties(randomProps);
+        }
+
+        // creates a random property of this player with a random value to set
+        if (GUILayout.Button("Set Player Property"))
+        {
+            Hashtable randomProps = new Hashtable();
+            randomProps[RandomCustomPlayerProp()] = Random.Range(0, 99);
+            this.GameInstance.LocalPlayer.SetCustomProperties(randomProps);
+        }
+
+        if (GUILayout.Button("Clear Ev Cache (all)"))
+        {
+            this.GameInstance.ClearAllTileClickEv();
+        }
+        
+        if (GUILayout.Button("Clear Ev Cache (turn)  " + (this.GameInstance.turnNumber-1)))
+        {
+            this.GameInstance.ClearTileClickEvForTurn(this.GameInstance.turnNumber-1);
+        }
+
+        if (GUILayout.Button("hide+close"))
+        {
+            this.GameInstance.CurrentRoom.IsOpen = false;
+            this.GameInstance.CurrentRoom.IsVisible = false;
+        }
+        
+        GUI.color = this.playfabComponent.pf_orange;
+		if (GUILayout.Button("Win MVP"))
+		{
+			this.playfabComponent.AwardMVP();
+		}
+		if (GUILayout.Button("Give Experience"))
+		{
+			this.playfabComponent.GrantExperience();
+		}
+		GUI.color = Color.white;
+		
+		GUILayout.FlexibleSpace();
+		
+		if (GUILayout.Button("Leave (return later)"))
+        {
+            this.GameInstance.OpLeaveRoom(true);
+			
+			// PlayFab
+			this.playfabComponent.LeaveGame();
+		}
+		if (GUILayout.Button("Abandon"))
+        {
+            this.GameInstance.OpLeaveRoom(false);
+           
+           	// PlayFab
+			this.playfabComponent.LeaveGame();
+		}
+        GUILayout.EndArea();
+    }
+
+    private string FormatRoomProps()
+    {
+        Hashtable customRoomProps = this.GameInstance.CurrentRoom.CustomProperties;
+        string interestingProps = "";
+        foreach (string propName in GameInstance.roomProps)
+        {
+            if (customRoomProps.ContainsKey(propName))
+            {
+                if (!string.IsNullOrEmpty(interestingProps)) interestingProps += " ";
+                interestingProps += propName + ":" + customRoomProps[propName];
+            }
+        }
+        return interestingProps;
+    }
+
+    private string RandomCustomRoomProp()
+    {
+        string[] roomProps = GameInstance.roomProps;
+        return roomProps[Random.Range(0, roomProps.Length)];
+    }
+
+    private string RandomCustomPlayerProp()
+    {
+        string[] playerProps = GameInstance.playerProps;
+        return playerProps[Random.Range(0, playerProps.Length)];
+    }
+}
---
+++ turnbased_sdk/Assets/Demo/UtilityScripts/PlayFabScripts/PlayFabIntegration.cs
@@ -0,0 +1,312 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using ExitGames.Client.Photon.LoadBalancing;
+using PlayFab;
+using PlayFab.ClientModels;
+using PlayFab.Internal;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+public class PlayFabIntegration : MonoBehaviour {
+	public int gui_depth = 0;
+	public Texture2D pf_logo;
+	public Rect logoDims;
+	
+	public DemoGUI photonComponent; 
+	
+	// using PlayFab testing account and title
+	const string deviceId = "a931f26995380c5a";
+	const string playfabTitleId = "FD3A";
+	
+	public enum GuiStates {off=0, login=1, createRoom=2, customEvents=3, loading=4 }
+	public GuiStates activeState = GuiStates.off; 
+	
+	
+	private bool isAuthenticated = false;
+	private bool isJoined = false;
+	public bool hideTips = false;
+	private Dictionary<string, int> userStats = new Dictionary<string, int>();
+	private string team = string.Empty;
+	private string playfabId = string.Empty;
+	public Color32 pf_orange = new Color32(250, 130,0, 255);
+	public Color32 pf_blue = new Color32(0, 173,239,255);
+	
+	// Use this for initialization
+	void Start()
+	{
+		
+		PlayFabSettings.TitleId = playfabTitleId;
+		this.activeState = GuiStates.login;
+	}
+	
+	void OnGUI()
+	{
+		if(photonComponent.GameInstance != null)
+		{
+			if(photonComponent.GameInstance.State == ExitGames.Client.Photon.LoadBalancing.ClientState.JoinedLobby && this.isAuthenticated == false)
+			{
+				this.isAuthenticated = true;
+				this.activeState = GuiStates.createRoom;
+				StartCoroutine(GetUserData(1.0f));
+			}
+			else if(photonComponent.GameInstance.State == ExitGames.Client.Photon.LoadBalancing.ClientState.Joined && this.isJoined == false)
+			{
+				this.isJoined = true;		
+				this.activeState = GuiStates.customEvents;
+				StartCoroutine(GetUserData(1.0f));
+			}
+		}
+		
+//	
+		if(this.activeState != GuiStates.off)
+		{
+			GUI.depth = this.gui_depth;
+//			
+			if(pf_logo != null && this.hideTips == false)
+			{	
+				GUI.Box(new Rect(0, Screen.height * .75f, Screen.width, Screen.height*25f), "");
+				GUI.Box(new Rect(0, Screen.height * .75f, Screen.width, Screen.height*25f), "");
+				GUI.Box(new Rect(0, Screen.height * .75f, Screen.width, Screen.height*25f), "");
+				GUI.Box(new Rect(0, Screen.height * .75f, Screen.width, Screen.height*25f), "");
+				GUI.DrawTexture(new Rect(5, Screen.height - this.logoDims.height, this.logoDims.width, this.logoDims.height), this.pf_logo);	
+					
+			}
+//			
+			if(this.activeState == GuiStates.createRoom || this.activeState == GuiStates.customEvents || this.activeState == GuiStates.loading)
+			{
+				Rect statsRect = new Rect(Screen.width*.8f -5, Screen.height*.75f, Screen.width*.25f, Screen.height*.25f);
+				GUI.Box(statsRect, "");
+				
+				GUILayout.BeginArea(statsRect);
+					string redTeamValue =  this.userStats.ContainsKey("RedTeamJoinedCount") ? this.userStats["RedTeamJoinedCount"].ToString() : "...";
+					string bluTeamValue =  this.userStats.ContainsKey("BluTeamJoinedCount") ? this.userStats["BluTeamJoinedCount"].ToString() : "...";
+					string mvpValue =  this.userStats.ContainsKey("MVPsWonCount") ? this.userStats["MVPsWonCount"].ToString() : "...";
+					string expValue =  this.userStats.ContainsKey("ExperiencePoints") ? this.userStats["ExperiencePoints"].ToString() : "...";
+					
+						GUI.color = this.pf_orange;
+						GUILayout.BeginHorizontal();
+						GUILayout.Label("PlayFab Player Stats:");
+						GUILayout.FlexibleSpace();
+					GUILayout.EndHorizontal();
+					GUI.color = this.pf_blue;
+					GUILayout.Label(string.Format("RedTeamJoinedCount: {0}", redTeamValue));
+					GUILayout.Label(string.Format("BluTeamJoinedCount: {0}", bluTeamValue));
+					GUILayout.Label(string.Format("MVPsWonCount: {0}", mvpValue));
+					GUILayout.Label(string.Format("ExperiencePoints: {0}", expValue));
+					GUI.color = Color.white;
+				GUILayout.EndArea();
+			}
+//			
+//			GUILayout.BeginArea(new Rect(5,Screen.height*.85f, Screen.width*.5f, Screen.height*.15f));
+//				GUILayout.BeginHorizontal();
+//					GUILayout.Label("Photon Connection Status:");
+//				GUILayout.EndHorizontal();
+//				
+//				GUILayout.Label(string.Format("IsConnected: {0}, IsReady: {1}", PhotonNetwork.connected.ToString(), PhotonNetwork.connectedAndReady.ToString()));
+//				GUILayout.Label(string.Format("ConnectionState: {0}", PhotonNetwork.connectionStateDetailed.ToString()));
+//				if(PhotonNetwork.room != null)
+//				{
+//					GUILayout.Label(string.Format("CurrentRoom: {0}", PhotonNetwork.room.name));
+//				}
+//			GUILayout.EndArea();
+//
+			Rect centralBox = new Rect();
+			
+			if(hideTips == false)
+			{
+				switch(this.activeState)
+				{
+					case GuiStates.login:
+						centralBox = new  Rect(Screen.width/4, Screen.height*.75f, Screen.width/2, Screen.height*.25f);
+						
+						 GUILayout.BeginArea(centralBox);
+							GUI.color = this.pf_orange;
+							GUILayout.Label("Photon Custom Authentication with Playfab Credentials:");
+							GUI.color = Color.white;
+							GUILayout.Label("For the purposes of this demo, we will be using a testing device id for the login credentials. We provide these credentials to Photon, which then validates and permits entry into the MasterServer--MainLobby. From the main lobby a user can create and join rooms and games. Doing so will in turn fire the web hook associated with the player action.");
+							GUILayout.Label("");
+							//GUILayout.Label("In the bottom left you fill find the Photon Client Connection Status. You will see this box update to reflect the many changes in the client state,  Click below to contine.");
+							//GUILayout.Label("");
+							GUI.color = Color.white;
+						GUILayout.EndArea();	
+					break;
+					
+					case GuiStates.createRoom:
+						centralBox = new  Rect(Screen.width/4, Screen.height*.75f, Screen.width/2, Screen.height*.25f);
+						
+						GUILayout.BeginArea(centralBox);	
+							GUI.color = this.pf_orange;
+							GUILayout.Label("PlayFab Authentication Successful!");
+							GUI.color = Color.white;
+							GUILayout.Label("In the bottm right you can see the most up-to-date PlayFab PlayerStatistics for this user. You will see these stats change in real time as web hooks are called to process game logic.");
+							//GUILayout.Label("");
+							GUILayout.Label("From the MainLobby, a player can join existing rooms or create a new one. When you create and join a room our cloud script is randomly assigning the player to a team (Red or Blu) and tracking the results in Player Stats. Watch the stats to see on which team you are assigned.");
+							GUI.color = Color.white;
+						GUILayout.EndArea();	
+					break;
+					
+					case GuiStates.customEvents:
+						centralBox = new  Rect(Screen.width/4, Screen.height*.75f, Screen.width/2, Screen.height*.25f);
+						
+						GUILayout.BeginArea(centralBox);
+							GUI.color = this.pf_orange;
+							GUILayout.Label(string.Format("You are now in the game, welcome to the {0} team", this.team));
+							GUI.color = Color.white;
+							GUILayout.Label("While the game is running your loops and mechanics, custom events can be raised to augment your logic flow. These events can have custom input parameters as well as access to the basic Photon room details. With a event system this flexible, nearly every game can benifit.");
+							GUILayout.Label("");
+							GUI.color = Color.white;
+							
+							if(GUILayout.Button("Ok, I got it (hide these tips)"))
+							{
+								this.hideTips = true;
+							}
+							
+							GUILayout.Label("");
+						GUILayout.EndArea();	
+					break;
+					
+					default:
+						GUILayout.BeginArea(centralBox);
+							GUILayout.Label("Awaiting Server...");
+						GUILayout.EndArea();
+					break;
+				}	
+			}
+		}
+	}
+	
+	
+	public void AwardMVP()
+	{
+		// Trigger Custom Event
+		photonComponent.GameInstance.OpRaiseEvent(
+			eventCode: 15,
+			customEventContent: new Hashtable() { { "eventType", "mvp" }},
+			sendReliable: true,
+			raiseEventOptions: new RaiseEventOptions()
+			{
+				ForwardToWebhook = true,
+			});
+			StartCoroutine(GetUserData(1.25f));	
+	}
+	
+	public void GrantExperience()
+	{
+		//Trigger Custom Event
+		int expAward = UnityEngine.Random.Range(5000,10000);
+		Debug.Log("EXP: " + expAward);
+		photonComponent.GameInstance.OpRaiseEvent(
+			eventCode: 15,
+		customEventContent: new Hashtable() { { "eventType", "exp" }, { "amt", expAward } },
+		sendReliable: true,
+		raiseEventOptions: new RaiseEventOptions()
+		{
+			ForwardToWebhook = true,
+		});
+		StartCoroutine(GetUserData(1.25f));
+	}
+	
+	
+	public void LoginToPlayFab()
+	{
+		Debug.Log("Using demo device id");
+		LoginWithAndroidDeviceIDRequest request = new LoginWithAndroidDeviceIDRequest();
+		request.AndroidDeviceId = deviceId;
+		request.TitleId = playfabTitleId;
+		request.CreateAccount = true;
+		PlayFabClientAPI.LoginWithAndroidDeviceID(request, OnLoginSuccess, OnPlayFabError);
+		this.activeState = GuiStates.loading;
+	}
+	
+	void OnLoginSuccess(LoginResult result)
+	{
+		//Debug.Log(result.SessionTicket);
+		StartCoroutine(GetUserData());
+		this.playfabId = result.PlayFabId;
+		GetPhotonAuthenticationTokenRequest request = new GetPhotonAuthenticationTokenRequest();
+		request.PhotonApplicationId = photonComponent.AppId.Trim();
+		PlayFabClientAPI.GetPhotonAuthenticationToken(request, OnPhotonAuthenticationSuccess, OnPlayFabError);
+	}
+	
+	void OnPhotonAuthenticationSuccess(GetPhotonAuthenticationTokenResult result)
+	{
+		//Debug.Log (string.Format("pfid:{0}, photon:{1}", this.playfabId, result.PhotonCustomAuthenticationToken));
+		photonComponent.ConnectToMasterServer(this.playfabId, result.PhotonCustomAuthenticationToken);
+	}
+	
+	IEnumerator GetUserData(float sec = 0)
+	{
+		yield return new WaitForSeconds(sec);
+		GetUserStatisticsRequest request = new GetUserStatisticsRequest();
+		PlayFabClientAPI.GetUserStatistics(request, OnGetUserDataSuccess, OnPlayFabError);
+	}
+	
+	void OnGetUserDataSuccess(GetUserStatisticsResult result)
+	{
+		if(result.UserStatistics.ContainsKey("BluTeamJoinedCount") && this.userStats.ContainsKey("BluTeamJoinedCount"))
+		{
+			if(result.UserStatistics["BluTeamJoinedCount"] > this.userStats["BluTeamJoinedCount"] )
+			{
+				this.team = "Blu";
+				Debug.Log(result.UserStatistics["BluTeamJoinedCount"] + " : " + this.userStats["BluTeamJoinedCount"]);
+			} 
+		}
+		else if(result.UserStatistics.ContainsKey("BluTeamJoinedCount") && !this.userStats.ContainsKey("BluTeamJoinedCount"))
+		{
+			if(this.userStats.Count != 0)
+			{
+				this.team = "Blu";
+				Debug.Log(result.UserStatistics["BluTeamJoinedCount"] + " : blue" );
+			}
+		}
+		
+		if(result.UserStatistics.ContainsKey("RedTeamJoinedCount") && this.userStats.ContainsKey("RedTeamJoinedCount"))
+		{
+			if(result.UserStatistics["RedTeamJoinedCount"] > this.userStats["RedTeamJoinedCount"] )
+			{
+				this.team = "Red";
+				Debug.Log(result.UserStatistics["RedTeamJoinedCount"] + " : " + this.userStats["RedTeamJoinedCount"]);
+			} 
+		}
+		else if(result.UserStatistics.ContainsKey("RedTeamJoinedCount") && !this.userStats.ContainsKey("RedTeamJoinedCount"))
+		{
+			if(this.userStats.Count != 0)
+			{
+				this.team = "Red";
+				Debug.Log(result.UserStatistics["RedTeamJoinedCount"] + " : red");
+			}
+		}
+		
+		this.userStats = result.UserStatistics;
+	}
+	
+	void OnPlayFabError(PlayFabError error)
+	{
+		Debug.Log(error.ErrorMessage);
+	}
+
+//	public void SetStateAuthInput()
+//	{
+//		this.activeState = GuiStates.login;
+//	}
+	
+	public void OnLogOut()
+	{
+		this.isAuthenticated = false;
+		this.isJoined = false;
+		this.activeState = GuiStates.login;
+		this.team = string.Empty;
+		this.hideTips = false;
+	}
+	
+	public void LeaveGame()
+	{
+		this.isAuthenticated = false;
+		this.isJoined = false;
+		this.activeState = GuiStates.loading;
+		this.team = string.Empty;
+		this.hideTips = false;
+	}
+}
+
